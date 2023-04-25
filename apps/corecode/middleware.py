84a47f46836b0a129803e3f7e90800fb2f2588f5
EC2 Default User <ec2-user@ip-172-31-42-186.eu-west-3.compute.internal>
2023-04-25 16:03:27 +0000

    test

--- apps/corecode/middleware.py
+++ apps/corecode/middleware.py
@@ -6,10 +6,10 @@ class SiteWideConfigs:
         self.get_response = get_response
 
     def __call__(self, request):
-        current_session = AcademicSession.objects.get(current=True)
+        #current_session = AcademicSession.objects.get(current=True)
         current_term = AcademicTerm.objects.get(current=True)
 
-        request.current_session = current_session
+        #request.current_session = current_session
         request.current_term = current_term
 
         response = self.get_response(request)
